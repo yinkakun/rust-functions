abbe4f7f72be6ae7891360f3b7003cfbebe7d1cf
yinkakun <yinkakun@gmail.com>
2023-01-28 13:34:02 +0100

    function with a return value

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
     println!("Hello, world!");
     another_function(5);
+    print_measurement(20, 30);
+    let x = return_number(800);
+    println!("x is {x}")
 }
 
 fn another_function(x: u32) {
     println!("x is {x}")
+}
+
+fn print_measurement(h: u32, w: u32) {
+    println!("height: {h}, width: {w}");
+}
+
+fn return_number(num: u32) -> u32 {
+    num
 }
